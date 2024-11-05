88d5b0c55702d22399a56607b7ba767168041459
Danila200324 <danven2018@mail.ru>
2024-11-05 22:17:48 +0100

    adding contact_info page

--- insurance/accounts/urls.py
+++ insurance/accounts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
          auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
          name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
-         name='password_reset_complete'),
+         name='password_reset_complete')
 
 ]
--- insurance/accounts/views.py
+++ insurance/accounts/views.py
@@ -74,3 +74,14 @@ def reset_password(request):
     else:
         form = PasswordResetForm()
     return render(request, 'password_reset.html', {'form': form})
+
+
+def contact_info_view(request):
+    company_info = {
+        'name': 'AegisAuto',
+        'email': 'contact_aegis@gmail.com',
+        'phone': '+48-98-567-657',
+        'address': 'ul. Kwiatowa 29 , Warsaw, Poland'
+    }
+
+    return render(request, 'contact_info.html', {'company': company_info})
